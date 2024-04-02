23db926deb0af417248232ba3508778e68d59ccf
Yanuanua <164603340+Yanuanua@users.noreply.github.com>
2024-04-02 19:04:26 +0800

    Update explore.py

--- explore.py
+++ explore.py
@@ -114,9 +114,8 @@ if selected_option == 'Method 1: For travelers who have time constraints and rel
     for spot, info in spots_locations.items():
         folium.Marker(
             location=[info["latitude"], info["longitude"]],
-            popup=f"{spot} for {info['themes']}",
-            #tooltip=tooltip,
-            tooltip=spot
+            #popup=f"{spot} for {info['themes']}",
+            tooltip=f"{spot} for {info['themes']}"
         ).add_to(m)
     points=[(22.3203648,114.169773),(22.2823565,114.1886969)]
     folium.PolyLine(points, color="blue", weight=2.5, opacity=1).add_to(m)
