8701be9f5f7d3114b4b677ab3aa2916d32c83038
Yanuanua <164603340+Yanuanua@users.noreply.github.com>
2024-04-03 19:04:08 +0800

    Update explore.py

--- explore.py
+++ explore.py
@@ -5,6 +5,7 @@ from streamlit_folium import folium_static
 import pandas as pd
 import openpyxl
 
+#Method1方法
 def plan_route(start_spot, total_spots, max_time, themes):
     current_spot = start_spot
     route = [current_spot]
@@ -61,39 +62,25 @@ def plan_route(start_spot, total_spots, max_time, themes):
     
     return route, total_time
 
-spots = {
-    'aa': {'time': 2, 'themes': ['自然', '休闲']},
-    'bb': {'time': 1, 'themes': ['人文', '城市']},
-    'cc': {'time': 3, 'themes': ['购物', '休闲']},
-    'dd': {'time': 2, 'themes': ['美食', '自然']},
-    'ee': {'time': 1, 'themes': ['宗教', '自然']},
-    'ff': {'time': 2, 'themes': ['游乐园']},
-    'gg': {'time': 2, 'themes': ['体育', '休闲']},
-    'hh': {'time': 3, 'themes': ['自然']},
-    'ii': {'time': 2, 'themes': ['人文', '城市']},
-    'jj': {'time': 1, 'themes': ['购物', '人文']},
-    'kk': {'time': 2, 'themes': ['美食', '休闲']},
-    'll': {'time': 1, 'themes': ['宗教']},
-    'mm': {'time': 2, 'themes': ['游乐园', '城市']},
-    'nn': {'time': 3, 'themes': ['体育', '购物']}
-}
-probabilities = {
-    'aa': {'bb': 0.35, 'cc': 0.2, 'dd': 0.15, 'ee': 0.45, 'ff': 0.25, 'gg': 0.3, 'hh': 0.12, 'ii': 0.35, 'jj': 0.18, 'kk': 0.42, 'll': 0.1, 'mm': 0.33, 'nn': 0.22},
-    'bb': {'aa': 0.25, 'cc': 0.3, 'dd': 0.1, 'ee': 0.5, 'ff': 0.2, 'gg': 0.35, 'hh': 0.1, 'ii': 0.4, 'jj': 0.22, 'kk': 0.48, 'll': 0.1, 'mm': 0.35, 'nn': 0.18},
-    'cc': {'aa': 0.2, 'bb': 0.25, 'dd': 0.12, 'ee': 0.42, 'ff': 0.2, 'gg': 0.3, 'hh': 0.1, 'ii': 0.38, 'jj': 0.2, 'kk': 0.45, 'll': 0.1, 'mm': 0.3, 'nn': 0.25},
-    'dd': {'aa': 0.22, 'bb': 0.2, 'cc': 0.1, 'ee': 0.48, 'ff': 0.18, 'gg': 0.32, 'hh': 0.1, 'ii': 0.42, 'jj': 0.2, 'kk': 0.4, 'll': 0.1, 'mm': 0.3, 'nn': 0.25},
-    'ee': {'aa': 0.18, 'bb': 0.28, 'cc': 0.15, 'dd': 0.5, 'ff': 0.2, 'gg': 0.35, 'hh': 0.1, 'ii': 0.4, 'jj': 0.18, 'kk': 0.45, 'll': 0.1, 'mm': 0.32, 'nn': 0.22},
-    'ff': {'aa': 0.2, 'bb': 0.25, 'cc': 0.1, 'dd': 0.52, 'ee': 0.22, 'gg': 0.3, 'hh': 0.1, 'ii': 0.4, 'jj': 0.2, 'kk': 0.45, 'll': 0.1, 'mm': 0.3, 'nn': 0.25},
-    'gg': {'aa': 0.2, 'bb': 0.23, 'cc': 0.1, 'dd': 0.5, 'ee': 0.25, 'ff': 0.3, 'hh': 0.1, 'ii': 0.38, 'jj': 0.2, 'kk': 0.42, 'll': 0.1, 'mm': 0.32, 'nn': 0.23},
-    'hh': {'aa': 0.2, 'bb': 0.22, 'cc': 0.1, 'dd': 0.48, 'ee': 0.2, 'ff': 0.3, 'gg': 0.1, 'ii': 0.42, 'jj': 0.2, 'kk': 0.45, 'll': 0.1, 'mm': 0.3, 'nn': 0.25},
-    'ii': {'aa': 0.2, 'bb': 0.23, 'cc': 0.1, 'dd': 0.5, 'ee': 0.25, 'ff': 0.3, 'gg': 0.1, 'hh': 0.4, 'jj': 0.2, 'kk': 0.45, 'll': 0.1, 'mm': 0.32, 'nn': 0.23},
-    'jj': {'aa': 0.2, 'bb': 0.25, 'cc': 0.1, 'dd': 0.48, 'ee': 0.22, 'ff': 0.3, 'gg': 0.3, 'hh': 0.38, 'ii': 0.2, 'kk': 0.45, 'll': 0.1, 'mm': 0.3, 'nn': 0.25},
-    'kk': {'aa': 0.2, 'bb': 0.22, 'cc': 0.1, 'dd': 0.5, 'ee': 0.25, 'ff': 0.3, 'gg': 0.1, 'hh': 0.42, 'ii': 0.2, 'jj': 0.45, 'll': 0.1, 'mm': 0.32, 'nn': 0.23},
-    'll': {'aa': 0.2, 'bb': 0.23, 'cc': 0.1, 'dd': 0.5, 'ee': 0.25, 'ff': 0.3, 'gg': 0.1, 'hh': 0.4, 'ii': 0.2, 'jj': 0.45, 'kk': 0.1, 'mm': 0.32, 'nn': 0.22},
-    'mm': {'aa': 0.2, 'bb': 0.25, 'cc': 0.1, 'dd': 0.48, 'ee': 0.22, 'ff': 0.3, 'gg': 0.1, 'hh': 0.42, 'ii': 0.2, 'jj': 0.45, 'kk': 0.1, 'll': 0.3, 'nn': 0.23},
-    'nn': {'aa': 0.2, 'bb': 0.23, 'cc': 0.1, 'dd': 0.5, 'ee': 0.25, 'ff': 0.3, 'gg': 0.1, 'hh': 0.4, 'ii': 0.2, 'jj': 0.45, 'kk': 0.1, 'll': 0.32, 'mm': 0.22}
-}
+#获取Method1数据
+df1 = pd.read_excel("Route Probabilities.xlsx", sheet_name='Sheet1')
+spots = {}
+probabilities = {}
+for index, row in df1.iterrows():
+    themes = [theme.strip() for theme in row['themes'].split(',')]
+    spot_data = {
+        'latitude': row['latitude'],
+        'longitude': row['longitude'],
+        'time': int(row['time']),
+        'cluster': str(row['cluster']),
+        'themes': themes
+    }
+    spots[row['spot_name']] = spot_data
+for index, row in df1.iterrows():
+    probs = {key: row[key] for key in row.index[6:38]}  # 提取从第7列到第38列的概率数据
+    probabilities[row['spot_name']] = probs
 
+#用户界面
 st.title('Plan Your HK Travel Route ^_^')
 selected_option = st.selectbox('Please choose one method for travel route planning.', ['Method 1: For travelers who have time constraints and rely on historical experience.', 'Method 2: For travelers who have time to spare and love to explore new spots.'])
 
@@ -101,9 +88,9 @@ selected_option = st.selectbox('Please choose one method for travel route planni
 if selected_option == 'Method 1: For travelers who have time constraints and rely on historical experience.':
     st.subheader('Method 1')
 
-    df = pd.read_excel("Spot Information.xlsx", sheet_name='Sheet1')
+    df2 = pd.read_excel("Spot Information.xlsx", sheet_name='Sheet1')
     Spots_Information = {}
-    for index, row in df.iterrows():
+    for index, row in df2.iterrows():
         themes = [theme.strip() for theme in row['theme'].split(',')]
         spot_data = {
             'latitude': row['latitude'],
