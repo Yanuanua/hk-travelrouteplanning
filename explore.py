1b3c8bfd633b7dc39336e2c0a43e59c722a915f5
Yanuanua <164603340+Yanuanua@users.noreply.github.com>
2024-04-06 15:59:04 +0800

    Update explore.py

--- explore.py
+++ explore.py
@@ -326,7 +326,7 @@ elif selected_ratio == 0:
     start_spot = st.text_input("Please enter your current residence as your starting point and return point (It is recommended to enter the hotel where you live in Hong Kong or the customs port to Hong Kong.")
     spots_num = st.slider('Please choose the number of spots you would like to visit.', min_value=1, max_value=len(spots))
     themes = st.multiselect('Please choose the theme\(s\) you are interested in \(more than one can be chosen\).', list(set(theme for spot in spots.values() for theme in spot['themes'])))
-    themes_num = st.slider('Please select how many times you would like the selected theme\(s\) to appear on the route.', min_value=1, max_value=spots_num)
+    themes_num = st.slider('Please select how many times you would like the selected theme\(s\) to appear on the route.', min_value=1, max_value=spots_num+1)
     if st.button('Generate your travel route!'):
         result = route_method2(start_point, spots_num, themes, themes_num)
         st.balloons()
