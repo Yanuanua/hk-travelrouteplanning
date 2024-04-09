a2bc5e0ed101e5bbfbb5aa0dbf55d5f2a678ef99
Yanuanua <164603340+Yanuanua@users.noreply.github.com>
2024-04-09 21:52:26 +0800

    Update explore.py

--- explore.py
+++ explore.py
@@ -259,14 +259,14 @@ def route_method3(start_point, spots_num, themes, themes_num, spots=spots): #必
 
 #用户界面
 st.title('Plan Your HK Travel Route ^_^')
-st.write('Hello dear travelers, welcome to our Hong Kong travel route recommendation system.')
+st.write('Hello dear tourists, welcome to our Hong Kong travel route recommendation system.')
 st.write('We now support 3 route recommendation methods.')
 st.markdown("<h1 style='text-align: left; color: green; font-size: 20px;'>Method 1 (if transition probability ≠ 0)</h1>", unsafe_allow_html=True)
-st.write('Suitable for travelers who follow popular attractions but have limited time, are in the attractions, or have certain intentions \(the larger the number, the more dependent it is on the transition probability, and the smaller the number, the more dependent it is on the landmark probability\).')
+st.write('Suitable for tourists who follow popular attractions but have limited time, are in the attractions, or have certain intentions \(the larger the number, the more dependent it is on the transition probability, and the smaller the number, the more dependent it is on the landmark probability\).')
 st.markdown("<h1 style='text-align: left; color: green; font-size: 20px;'>Method 2 (if transition probability = 0 and landmark probability ≠ 0)</h1>", unsafe_allow_html=True)
-st.write('Suitable for travelers who follow popular attractions and have ample time.')
+st.write('Suitable for tourists who follow popular attractions and have ample time.')
 st.markdown("<h1 style='text-align: left; color: green; font-size: 20px;'>Method 3 (if transition probability = 0 and landmark probability = 0)</h1>", unsafe_allow_html=True)
-st.write('Suitable for travelers who are willing to explore different attractions and have ample time.')
+st.write('Suitable for tourists who are willing to explore different attractions and have ample time.')
 options = [None, 0.00, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 1.00]
 selected_tratio = st.slider('Please choose the weight of the transition probability.', min_value=0.00, max_value=100.00, format="%.2f")
 selected_sratio = st.slider('Please choose the weight of the landmark probability.', min_value=0.00, max_value=100.00, format="%.2f")
