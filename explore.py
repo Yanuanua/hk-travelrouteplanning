154f2f8bbc6fa69b398069cf18fc3ce46b304c5c
Yanuanua <164603340+Yanuanua@users.noreply.github.com>
2024-04-11 22:41:28 +0800

    Update explore.py

--- explore.py
+++ explore.py
@@ -370,15 +370,13 @@ else:
             folium.Marker(
                 location= list(result[3][1]),
                 tooltip=f"Your Starting and Return Point: {result[2]}",
-                icon=folium.Icon(icon='0')
+                icon=folium.Icon(icon='cloud')
                 ).add_to(m_1)
-            i=1
             for spot in result[0][1:-1]:
                 folium.Marker(
                     location=[Spots_Information[spot]['latitude'], Spots_Information[spot]['longitude']],
                     tooltip=f"{spot} for {', '.join(Spots_Information[spot]['themes'])}",
-                    icon=folium.Icon(icon=i)
+                    icon=folium.Icon(icon='cloud')
                 ).add_to(m_1)
-                i=i+1
             folium_static(m_1)
     
